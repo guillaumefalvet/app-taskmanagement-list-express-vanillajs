9fc1c4d9ffe6cd8c53987176220dcc8a8a596953
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-07-18 11:18:01 +0200

    :bug: fix: database url

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,2 @@
-export const apiBaseUrl =
-  import.meta.env.VITE_API_BASE_URL ||
-  "https://kanban-list-production.up.railway.app/";
+export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 ("Exceptionnelement, comme le back et le front sont sur le même serveur, on s'en sort ici avec un /");
