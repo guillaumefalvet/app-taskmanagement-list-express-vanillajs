51ce28ea1e93608c88e88589926b7c368e340c41
Guillaume Falvet <guillaume.falvet@outlook.com>
2023-07-18 11:15:57 +0200

    🔧 config: backend url

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,4 @@
-export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || "/"; "Exceptionnelement, comme le back et le front sont sur le même serveur, on s'en sort ici avec un /"
+export const apiBaseUrl =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://kanban-list-production.up.railway.app/";
+("Exceptionnelement, comme le back et le front sont sur le même serveur, on s'en sort ici avec un /");
